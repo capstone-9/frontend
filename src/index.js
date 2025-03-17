00498cfba678d6fad5c032384e82d513194954bc
pepper-sister <yuyu7123@naver.com>
2025-03-17 22:51:36 +0900

    환경 세팅

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Diary from "./pages/diary"
+
+const 페이지목록 = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/diary", element: <Diary /> }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<RouterProvider router={페이지목록}/>);
---
+++ src/pages/diary.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+import '../styles/diary.css'
+
+const Diary = () => {
+  return (
+    <>
+    <div>일기쓰기 페이지</div>
+    <Link to="/">홈화면</Link>
+    </>
+  )
+}
+
+export default Diary;
