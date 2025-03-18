a094055d93b7e088c2273663e54ce5f8c5509fb9
pepper-sister <yuyu7123@naver.com>
2025-03-18 23:57:41 +0900

    feat: Header 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { Link } from 'react-router-dom'
 import './styles/App.css';
+
+import { Link } from 'react-router-dom'
+import Header from './components/header'
 
 const App = () => {
   return (
     <>
+    <Header />
     <div>하루톡톡 웹사이트</div>
     <Link to="/diary">일기쓰기</Link>
     </>
