e1b0aee85738839cd9b32f49566e3335ca70b7db
pepper-sister <yuyu7123@naver.com>
2025-03-21 00:18:13 +0900

    feat: 다크모드 추가, 로고 클릭시 홈화면 이동 추가

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,39 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react"
+import { Link } from 'react-router-dom'
 import '../styles/header.css'
 
 const Header = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false)
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode)
+    if (!isDarkMode) {
+      document.body.classList.add('dark__mode')
+      document.body.classList.remove('light__mode')
+    } else {
+      document.body.classList.add('light__mode')
+      document.body.classList.remove('dark__mode')
+    }
+  }
+
   return (
     <header>
-      <div class="header__left">
-        <img class="header__left__logo" src="/logo.png" />
-        <div class="header__left__name">하루톡톡</div>
+      <div className="header__left">
+        <Link to="/">
+          <img className="header__left__logo" src="/logo.png" alt="Designed by Freepik" />
+        </Link>
+        <div className="header__left__name">하루톡톡</div>
       </div>
-      <div class="header__right">
-        <FontAwesomeIcon icon={faMoon} />
-        <FontAwesomeIcon icon={faSun} />
-        <button class="header__right__signin">로그인</button>
-        <button class="header__right__signup">회원가입</button>
+      <div className="header__right">
+        <FontAwesomeIcon
+          icon={isDarkMode ? faSun : faMoon}
+          className={`header__right__mode ${isDarkMode ? "visible" : ""}`}
+          onClick={toggleDarkMode}
+        />
+        <button className="header__right__signin">로그인</button>
+        <button className="header__right__signup">회원가입</button>
       </div>
     </header>
   )
