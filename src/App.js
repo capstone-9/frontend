e68b1be91731090fc37e6d25560a1e727d311419
pepper-sister <yuyu7123@naver.com>
2025-03-21 14:19:10 +0900

    feat: Footer 라우터 추가

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './styles/App.css';
 
 import { Link } from 'react-router-dom'
 import Header from './components/header'
+import Footer from './components/footer'
 
 const App = () => {
   return (
-    <>
-    <Header />
-    <div>하루톡톡 웹사이트</div>
-    <Link to="/diary">일기쓰기</Link>
-    </>
+    <div className="app__body">
+      <Header />
+      <div>하루톡톡 웹사이트</div>
+      <Link to="/diary">일기쓰기</Link>
+      <Footer />
+    </div>
   )
 }
 
